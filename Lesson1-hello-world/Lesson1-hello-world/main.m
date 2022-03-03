fc61384fd2c48267313bc24b06e46f61af30aa43
kingrake <kingrake@163.com>
2022-03-03 14:22:26 +0800

    First test
    First test test

---
+++ Lesson1-hello-world/Lesson1-hello-world/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lesson1-hello-world
+//
+//  Created by lo on 2022/3/2.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"你好，李晨，欢迎你的回归!");
+        NSLog(@"Hello，Lichen，Wellcome you back!");
+        
+    }
+    return 0;
+}
